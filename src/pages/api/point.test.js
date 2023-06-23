ba771aaa743761faab9510286a5df63cf313ad37
Fang Jin <windmaomao@gmail.com>
2023-06-23 17:13:25 -0400

    add test case for point reward calculation

---
+++ src/pages/api/point.test.js
@@ -0,0 +1,39 @@
+function point(amount) {
+  let total = amount;
+  let rewards = 0;
+
+  if (total > 100) {
+    rewards += (total - 100) * 2;
+    total = 100;
+  }
+
+  if (total > 50) {
+    rewards += (total - 50) * 1;
+  }
+
+  return rewards;
+}
+
+describe('point', () => {
+  it('should not reward under 50', () => {
+    expect(point(-1)).toBe(0);
+    expect(point(0)).toBe(0);
+    expect(point(10)).toBe(0);
+    expect(point(20)).toBe(0);
+    expect(point(25)).toBe(0);
+    expect(point(50)).toBe(0);
+  });
+
+  it('should reward between 50 and 100', () => {
+    expect(point(51)).toBe(1);
+    expect(point(55)).toBe(5);
+    expect(point(75)).toBe(25);
+    expect(point(100)).toBe(50);
+  });
+
+  it('should reward above 100', () => {
+    expect(point(101)).toBe(52);
+    expect(point(105)).toBe(60);
+    expect(point(120)).toBe(90);
+  });
+});
